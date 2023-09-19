1c5609702aa2fe4acbdca7b91049185b87d648b5
AnasAlhussain <anasalhussain.sweeden@hotmail.com>
2023-09-19 09:39:19 +0200

    Add project files.

---
+++ SUT23-Lektion-Github/Program.cs
@@ -0,0 +1,10 @@
+﻿namespace SUT23_Lektion_Github
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}
